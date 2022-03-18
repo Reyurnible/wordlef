a5c7ced45f2f3409cb1ce4a8ece0f422bb23ef03
reyurnible <reyurnible@gmail.com>
2022-03-18 19:16:51 +0900

    :recycle: Add Letter model

---
+++ lib/domain/Letter.dart
@@ -0,0 +1,34 @@
+enum Letter {
+  A,
+  B,
+  C,
+  D,
+  E,
+  F,
+  G,
+  H,
+  I,
+  J,
+  K,
+  L,
+  M,
+  N,
+  O,
+  P,
+  Q,
+  R,
+  S,
+  T,
+  U,
+  V,
+  W,
+  X,
+  Y,
+  Z,
+}
+
+extension LetterExt on Letter {
+  String get value {
+    return this.toString().split(".")[1];
+  }
+}
