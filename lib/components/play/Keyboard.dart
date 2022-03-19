67a41301b1e27a2480f046607986636d85efc5ac
reyurnible <reyurnible@gmail.com>
2022-03-19 11:05:47 +0900

    :+1: Add delete and enter key

--- lib/components/play/Keyboard.dart
+++ lib/components/play/Keyboard.dart
@@ -37,6 +37,11 @@ class Keyboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<Widget> bottomItems = [];
+    bottomItems.add(EnterKeyItem(() {}));
+    bottomItems.addAll(bottomKeyboardItems.map((l) => LetterKeyItem(l)));
+    bottomItems.add(DeleteKeyItem(() {}));
+
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
@@ -51,17 +56,17 @@ class Keyboard extends StatelessWidget {
           ),
           Row(
             mainAxisSize: MainAxisSize.min,
-            children: bottomKeyboardItems.map((l) => LetterKeyItem(l)).toList(),
+            children: bottomItems,
           ),
         ]);
   }
 }
 
 class LetterKeyItem extends StatelessWidget {
-  LetterKeyItem(this.letter, {this.pressedCallback});
+  LetterKeyItem(this.letter, {this.onPressed});
 
   final Letter letter;
-  final LetterKeyItemClickCallback? pressedCallback;
+  final LetterKeyItemClickCallback? onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -69,34 +74,70 @@ class LetterKeyItem extends StatelessWidget {
         child: ElevatedButton(
           child: Text(letter.value),
           style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.grey),
-            minimumSize: MaterialStateProperty.all(Size(32, 48)),
-            fixedSize: MaterialStateProperty.all(Size(32, 48)),
-            padding: MaterialStateProperty.all(EdgeInsets.zero)
-          ),
+              backgroundColor: MaterialStateProperty.all(Colors.grey),
+              minimumSize: MaterialStateProperty.all(Size(36, 48)),
+              fixedSize: MaterialStateProperty.all(Size(36, 48)),
+              padding: MaterialStateProperty.all(EdgeInsets.zero),
+              textStyle: MaterialStateProperty.all(
+                  TextStyle(fontSize: 14, fontWeight: FontWeight.w600))),
           onPressed: () {
-            pressedCallback?.call(letter);
+            onPressed?.call(letter);
           },
         ),
-        margin: EdgeInsets.all(2)
-    );
+        margin: EdgeInsets.all(2));
   }
 }
 
 typedef LetterKeyItemClickCallback = void Function(Letter letter);
 
 class EnterKeyItem extends StatelessWidget {
+  EnterKeyItem(this.onPressed);
+
+  final VoidCallback? onPressed;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+        child: ElevatedButton(
+          child: Text("ENTER"),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.grey),
+            minimumSize: MaterialStateProperty.all(Size(54, 48)),
+            fixedSize: MaterialStateProperty.all(Size(54, 48)),
+            padding: MaterialStateProperty.all(EdgeInsets.zero),
+            textStyle: MaterialStateProperty.all(
+                TextStyle(fontSize: 12, fontWeight: FontWeight.w600)),
+          ),
+          onPressed: () {
+            onPressed?.call();
+          },
+        ),
+        margin: EdgeInsets.all(2));
   }
 }
 
 class DeleteKeyItem extends StatelessWidget {
+  DeleteKeyItem(this.onPressed);
+
+  final VoidCallback? onPressed;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+        child: ElevatedButton(
+          child: Text("DEL"),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.grey),
+            minimumSize: MaterialStateProperty.all(Size(54, 48)),
+            fixedSize: MaterialStateProperty.all(Size(54, 48)),
+            padding: MaterialStateProperty.all(EdgeInsets.zero),
+            textStyle: MaterialStateProperty.all(
+                TextStyle(fontSize: 12, fontWeight: FontWeight.w600)),
+          ),
+          onPressed: () {
+            onPressed?.call();
+          },
+        ),
+        margin: EdgeInsets.all(2));
   }
 }
