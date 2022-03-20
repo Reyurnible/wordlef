2cf4a7a05d35ef132928f3a7f9fa430eaedac4f2
reyurnible <reyurnible@gmail.com>
2022-03-20 23:40:16 +0900

    :+1: Add game word check

--- lib/domain/game.dart
+++ lib/domain/game.dart
@@ -13,12 +13,17 @@ class Game {
     Letter.R,
     Letter.D,
   ];
+  late final List<Word> wordList;
+  late final Set<String> checkUpperWordSet;
+
   final GameBoard board = GameBoard();
   GameStatus status = GameStatus.loading;
-  late final List<Word> wordList;
 
   void start(List<Word> wordList) {
     this.wordList = wordList;
+    checkUpperWordSet =
+        wordList.map((element) => element.word.toUpperCase()).toSet();
+
     status = GameStatus.playing;
   }
 
@@ -40,28 +45,33 @@ class Game {
     if (!isPlaying()) {
       return false;
     }
-    if (board.checkCurrentLineFilled()) {
-      // Check answer.
-      if (listEquals(answer, board.getCurrentLineLetters())) {
-        // Success.
-        status = GameStatus.succeed;
-        debugPrint("Game Succeed!!!");
-      } else {
-        // Failure
-        if (!board.moveNextLine()) {
-          // LOOSE GAME
-          status = GameStatus.loosed;
-          debugPrint("Game Loosed!!!");
-        } else {
-          debugPrint("Move to next line. (Current: ${board.getCurrentLine()})");
-        }
-      }
-      return true;
-    } else {
+    if (!board.checkCurrentLineFilled()) {
       // Error no filled
       debugPrint("No filled line");
       return false;
     }
+    if (!checkUpperWordSet
+        .contains(board.getCurrentLineLetters().joinedValue.toUpperCase())) {
+      // Error not in word list
+      debugPrint("Not in word list");
+      return false;
+    }
+    // Check answer.
+    if (listEquals(answer, board.getCurrentLineLetters())) {
+      // Success.
+      status = GameStatus.succeed;
+      debugPrint("Game Succeed!!!");
+    } else {
+      // Failure
+      if (!board.moveNextLine()) {
+        // LOOSE GAME
+        status = GameStatus.loosed;
+        debugPrint("Game Loosed!!!");
+      } else {
+        debugPrint("Move to next line. (Current: ${board.getCurrentLine()})");
+      }
+    }
+    return true;
   }
 
   bool isPlaying() {
@@ -72,4 +82,3 @@ class Game {
     return status == GameStatus.succeed || status == GameStatus.loosed;
   }
 }
-
