e64ff86f098f5ef1444b15d1713b3222a303acac
reyurnible <reyurnible@gmail.com>
2022-03-29 10:20:11 +0900

    :+1: Add loading check

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -21,6 +22,10 @@ void main() {
         child: const MyApp(),
       ),
     );
+
+    // Show Loading
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
     await tester.pump();
 
     expect(find.widgetWithText(LetterSpot, "E"), findsNothing);
